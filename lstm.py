7217ebbe46ed3ddadc004a3cbcdab122017387bf
void <git.email.address@googlemail.com>
2018-10-19 19:04:27 +0200

    GPU usage is 3%, even after cuDNN parameter adjustments.

---
+++ lstm.py
@@ -0,0 +1,70 @@
+import tensorflow as tf
+
+from profiler import Profiler
+
+class LSTMNet:
+	def __init__(self, batch_count, frame_count, lstm_layers):
+		self.batch_count = batch_count
+		self.frame_count = frame_count
+		self.lstm_layers = lstm_layers
+
+		self.batch_size = batch_count * frame_count
+
+	def train(self, dry_training_wav, wet_training_wav, dry_validation_wav, wet_validation_wav):
+		graph = self.get_graph()
+		with tf.Session(graph = graph) as session:
+			initializer = tf.global_variables_initializer()
+			session.run(initializer)
+			iteration = 1
+			dry_data_placeholder = graph.get_tensor_by_name('dry_data:0')
+			wet_data_placeholder = graph.get_tensor_by_name('wet_data:0')
+			loss = graph.get_operation_by_name('loss')
+			minimize = graph.get_operation_by_name('minimize')
+			print('Commencing training.')
+			while True:
+				print(f'Iteration {iteration}')
+				self.run_operation(dry_training_wav, wet_training_wav, minimize, dry_data_placeholder, wet_data_placeholder, session)
+				losses = self.run_operation(dry_validation_wav, wet_validation_wav, loss, dry_data_placeholder, wet_data_placeholder, session)
+				validation_loss = sum(losses)
+				print(f'Validation: {validation_loss}')
+				iteration += 1
+
+	def get_graph(self):
+		graph = tf.Graph()
+		with graph.as_default():
+			batch_shape = [self.batch_size]
+			dry_data = tf.placeholder(tf.float32, batch_shape, 'dry_data')
+			wet_data = tf.placeholder(tf.float32, batch_shape, 'wet_data')
+
+			lstm = tf.contrib.cudnn_rnn.CudnnLSTM(self.lstm_layers, self.frame_count, name = "lstm")
+			reshaped_dry_data = tf.reshape(dry_data, [self.batch_count, 1, self.frame_count])
+			lstm_output, _ = lstm(reshaped_dry_data)
+			flat_lstm_output = tf.reshape(lstm_output, batch_shape)
+			prediction = tf.nn.elu(flat_lstm_output)
+
+			loss = tf.sqrt(tf.losses.mean_squared_error(prediction, wet_data), name = 'loss')
+			optimizer = tf.train.AdamOptimizer()
+			minimize = optimizer.minimize(loss, name = 'minimize')
+		return graph
+
+	def run_operation(self, dry_data, wet_data, operation, dry_data_placeholder, wet_data_placeholder, session):
+		offset = 0
+		output = []
+		profiler = Profiler()
+		while offset + self.batch_size < len(dry_data):
+			print(f'Progress: {offset}/{len(dry_data)}')
+			dry_batch = self.get_batch(dry_data, offset)
+			wet_batch = self.get_batch(wet_data, offset)
+			feed = {
+				dry_data_placeholder: dry_batch,
+				wet_data_placeholder: wet_batch
+			}
+			profiler.stop('overhead')
+			operation_output = session.run(operation, feed)
+			profiler.stop('kernel')
+			output.append(operation_output)
+			offset += self.batch_size
+		return output
+
+	def get_batch(self, data, offset):
+		return data[offset : offset + self.batch_size]
---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+
+import scipy.io.wavfile
+from lstm import LSTMNet
+
+def read_wav(path):
+	rate, data = scipy.io.wavfile.read(path)
+	return data
+
+if len(sys.argv) != 5:
+	print('Usage:')
+	print(f'{sys.argv[0]} <dry training WAV file> <wet training WAV file> <dry validation WAV file> <wet validation WAV file>')
+	sys.exit(1)
+
+dry_training_wav_path = sys.argv[1]
+wet_training_wav_path = sys.argv[2]
+
+dry_validation_wav_path = sys.argv[3]
+wet_validation_wav_path = sys.argv[4]
+
+dry_training_wav = read_wav(dry_training_wav_path)
+wet_training_wav = read_wav(wet_training_wav_path)
+
+dry_validation_wav = read_wav(dry_validation_wav_path)
+wet_validation_wav = read_wav(wet_validation_wav_path)
+
+if len(dry_training_wav) != len(wet_training_wav) or len(dry_validation_wav) != len(wet_validation_wav):
+	raise Exception('Dry and wet WAVs must be same length.')
+
+net = LSTMNet(1024, 96, 64)
+net.train(dry_training_wav, wet_training_wav, dry_validation_wav, wet_validation_wav)
