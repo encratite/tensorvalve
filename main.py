b8145275d772e40bfb186537824ee79d6482802a
void <git.email.address@googlemail.com>
2018-10-27 04:02:35 +0200

    Preparing parameter space exploration.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 
 import scipy.io.wavfile
-from lstm import LSTMNet
+import tensorflow as tf
+
+from tensorvalve import TensorValve
 from profiler import Profiler
 
 def read_wav(path):
@@ -9,9 +11,9 @@ def read_wav(path):
 	rate, data = scipy.io.wavfile.read(path)
 	return data
 
-if len(sys.argv) != 6:
+if len(sys.argv) != 5:
 	print('Usage:')
-	print(f'{sys.argv[0]} <dry training WAV file> <wet training WAV file> <dry validation WAV file> <wet validation WAV file> <session directory>')
+	print(f'{sys.argv[0]} <dry training WAV file> <wet training WAV file> <dry validation WAV file> <wet validation WAV file>')
 	sys.exit(1)
 
 dry_training_wav_path = sys.argv[1]
@@ -19,8 +21,6 @@ wet_training_wav_path = sys.argv[2]
 
 dry_validation_wav_path = sys.argv[3]
 wet_validation_wav_path = sys.argv[4]
-
-save_path = sys.argv[5]
 
 profiler = Profiler()
 
@@ -35,5 +35,16 @@ profiler.stop('Done loading WAV files.')
 if len(dry_training_wav) != len(wet_training_wav) or len(dry_validation_wav) != len(wet_validation_wav):
 	raise Exception('Dry and wet WAVs must be same length.')
 
-net = LSTMNet(32, 512, 96, 64, save_path)
-net.train(dry_training_wav, wet_training_wav, dry_validation_wav, wet_validation_wav)
+layer_types = ('lt', [('lstm', tf.contrib.cudnn_rnn.CudnnLSTM), ('gru', tf.contrib.cudnn_rnn.CudnnGRU)])
+time_steps = ('ts', [1, 4, 16, 64])
+batch_sizes = ('bs', [1, 4, 16, 64, 256, 512])
+frame_counts = ('fc', [32, 64, 128, 256])
+dropouts = ('dr', [0.0, 0.25, 0.5])
+rnn_bias_initializers = ('bi', [('zero', None), ('xavier', tf.contrib.layers.xavier_initializer())])
+activation_functions = ('af', [('elu', tf.nn.elu), ('softsign', tf.nn.softsign), ('tanh', tf.tanh)])
+learning_rates = ('lr', [0.001, 0.005, 0.01, 0.05])
+
+time_limit = 15 * 60
+
+# valve = TensorValve(32, 512, 96, 64, save_path)
+# valve.train(dry_training_wav, wet_training_wav, dry_validation_wav, wet_validation_wav)
--- tensorvalve.py
+++ tensorvalve.py
@@ -5,18 +5,34 @@ import tensorflow as tf
 
 from profiler import Profiler
 
-class LSTMNet:
-	def __init__(self, time_steps, batch_count, frame_count, lstm_layers, save_path):
-		self.time_steps = time_steps
-		self.batch_count = batch_count
-		self.frame_count = frame_count
-		self.lstm_layers = lstm_layers
+class TensorValve:
+	def __init__(self, layer_type = None, time_steps = None, batch_size = None, input_size = None, layers = None, dropout = None, rnn_bias_initializer = None, activation_function = None, learning_rate = None, time_limit = None, save_path = None):
+		assert layer_type != None
+		assert batch_size != None
+		assert input_size != None
+		assert layers != None
+		assert save_path != None
+		assert dropout != None
+		assert activation_function != None
+		assert learning_rate != None
 
+		self.layer_type = layer_type
+		self.time_steps = time_steps
+		self.batch_size = batch_size
+		self.input_size = input_size
+		self.layers = layers
+		self.dropout = dropout
+		self.rnn_bias_initializer = rnn_bias_initializer
+		self.activation_function = activation_function
+
+		self.learning_rate = learning_rate
+
+		self.time_limit = time_limit
 		prefix = os.path.basename(save_path)
 		self.save_path = os.path.join(save_path, prefix)
 		self.save_path_directory = save_path
 
-		self.batch_size = time_steps * batch_count * frame_count
+		self.batch_elements = time_steps * batch_size * input_size
 
 	def train(self, dry_training_wav, wet_training_wav, dry_validation_wav, wet_validation_wav):
 		graph = self.get_graph()
@@ -28,42 +44,53 @@ class LSTMNet:
 			if self.save_path_exists():
 				saver.restore(session, self.save_path)
 				profiler.stop('Restored model.')
+
 			epoch_variable = graph.get_tensor_by_name('epoch:0')
-			increment_epoch = graph.get_operation_by_name('increment_epoch')
 			dry_data_placeholder = graph.get_tensor_by_name('dry_data:0')
 			wet_data_placeholder = graph.get_tensor_by_name('wet_data:0')
 			loss = graph.get_tensor_by_name('loss:0')
+			loss_minimum = graph.get_tensor_by_name('loss_minimum:0')
+
 			minimize = graph.get_operation_by_name('minimize')
+			update_loss_minimum = graph.get_operation_by_name('update_loss_minimum')
+			increment_epoch = graph.get_operation_by_name('increment_epoch')
+
 			print('Commencing training.')
-			while True:
+			start = time.perf_counter()
+			while self.time_limit is None or time.perf_counter() - start < self.time_limit:
 				profiler = Profiler()
+				operations = [minimize, update_loss_minimum, increment_epoch]
 				self.run_operation(dry_training_wav, wet_training_wav, minimize, dry_data_placeholder, wet_data_placeholder, session)
-				session.run(increment_epoch)
 				epoch = session.run(epoch_variable)
 				profiler.stop(f'Completed epoch {epoch}.')
 				losses = self.run_operation(dry_validation_wav, wet_validation_wav, loss, dry_data_placeholder, wet_data_placeholder, session)
 				validation_loss = sum(losses)
 				profiler.stop(f'Loss: {validation_loss}')
-				self.save_session(session, saver)
-				profiler.stop('Saved model.')
+				if epoch % 10 == 0:
+					self.save_session(session, saver)
+					profiler.stop('Saved model.')
+			self.save_session(session, saver)
+			profiler.stop('Training time limit expired. Saved model.')
 
 	def get_graph(self):
 		graph = tf.Graph()
 		with graph.as_default():
 			print('Constructing graph.')
 			profiler = Profiler()
-			batch_shape = [self.batch_size]
+			batch_shape = [self.batch_elements]
 			dry_data = tf.placeholder(tf.float32, batch_shape, 'dry_data')
 			wet_data = tf.placeholder(tf.float32, batch_shape, 'wet_data')
 			epoch_variable = tf.get_variable('epoch', dtype = tf.int32, initializer = tf.constant(0))
 			increment_epoch = tf.assign(epoch_variable, epoch_variable + 1, name = 'increment_epoch')
-			lstm = tf.contrib.cudnn_rnn.CudnnLSTM(self.lstm_layers, self.frame_count, bias_initializer = tf.contrib.layers.xavier_initializer(), name = 'lstm')
-			reshaped_dry_data = tf.reshape(dry_data, [self.time_steps, self.batch_count, self.frame_count])
+			lstm = self.layer_type(self.layers, self.input_size, dropout = self.dropout, bias_initializer = self.rnn_bias_initializer, name = 'rnn')
+			reshaped_dry_data = tf.reshape(dry_data, [self.time_steps, self.batch_size, self.input_size])
 			lstm_output, _ = lstm(reshaped_dry_data)
 			flat_lstm_output = tf.reshape(lstm_output, batch_shape)
-			prediction = tf.nn.elu(flat_lstm_output)
+			prediction = self.activation_function(flat_lstm_output, name = 'prediction')
 			loss = tf.sqrt(tf.losses.mean_squared_error(prediction, wet_data), name = 'loss')
-			optimizer = tf.train.AdamOptimizer()
+			loss_minimum = tf.get_variable('loss_minimum', dtype = tf.float)
+			update_loss_minimum = tf.minimum(loss, loss_minimum, 'update_loss_minimum')
+			optimizer = tf.train.AdamOptimizer(learning_rate = self.learning_rate)
 			minimize = optimizer.minimize(loss, name = 'minimize')
 			profiler.stop('Done constructing graph.')
 		return graph
@@ -71,7 +98,7 @@ class LSTMNet:
 	def run_operation(self, dry_data, wet_data, operation, dry_data_placeholder, wet_data_placeholder, session):
 		offset = 0
 		output = []
-		while offset + self.batch_size < len(dry_data):
+		while offset + self.batch_elements < len(dry_data):
 			dry_batch = self.get_batch(dry_data, offset)
 			wet_batch = self.get_batch(wet_data, offset)
 			feed = {
@@ -80,11 +107,11 @@ class LSTMNet:
 			}
 			operation_output = session.run(operation, feed)
 			output.append(operation_output)
-			offset += self.batch_size
+			offset += self.batch_elements
 		return output
 
 	def get_batch(self, data, offset):
-		return data[offset : offset + self.batch_size]
+		return data[offset : offset + self.batch_elements]
 
 	def save_path_exists(self):
 		return os.path.exists(self.save_path_directory)
