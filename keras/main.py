401efe97b6d9cb179c5071eb7972fc7ba51498cc
void <git.email.address@googlemail.com>
2018-12-08 15:27:38 +0100

    Minor progress.

--- keras/main.py
+++ keras/main.py
@@ -1,24 +1,28 @@
+import os
 import random
 import sys
+
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 import numpy as np
 import scipy.io.wavfile
 from keras import Sequential
-from keras.layers import LSTM, GRU, Flatten
+from keras.layers import LSTM, GRU, Flatten, Reshape
 from keras.backend import square, mean, sqrt
 
 from profiler import Profiler
 
-def read_wav(path, shape=None):
+def read_wav(path, input_shape):
 	print(f'Loading WAV file from "{path}".')
 	rate, data = scipy.io.wavfile.read(path)
-	return data
+	reshaped_data = reshape_wav(data, input_shape)
+	return reshaped_data
 	
-def reshape_wav(data, sample_shape):
-	samples_per_batch = sample_shape[1] * sample_shape[2]
+def reshape_wav(data, input_shape):
+	samples_per_batch = input_shape[0] * input_shape[1]
 	batches = len(data) // samples_per_batch
 	resized_data = np.resize(data, batches * samples_per_batch)
-	reshaped_data = np.reshape(resized_data, sample_shape)
+	reshaped_data = np.reshape(resized_data, (-1, ) + input_shape)
 	return reshaped_data
 	
 def root_mean_squared_error(y_true, y_pred):
@@ -34,7 +38,7 @@ rnn_units = 96
 rnn_timesteps = 64
 rnn_features = rnn_units
 
-batch_size = 1
+batch_size = 128
 epochs = 10
 	
 x_training_path = sys.argv[1]
@@ -45,27 +49,23 @@ y_validation_path = sys.argv[4]
 
 profiler = Profiler()
 
-x_training_raw = read_wav(x_training_path)
-y_training = read_wav(y_training_path)
+input_shape = (rnn_timesteps, rnn_features)
 
-x_validation_raw = read_wav(x_validation_path)
-y_validation = read_wav(y_validation_path)
+x_training = read_wav(x_training_path, input_shape)
+y_training = read_wav(y_training_path, input_shape)
+
+x_validation = read_wav(x_validation_path, input_shape)
+y_validation = read_wav(y_validation_path, input_shape)
 
 profiler.stop('Done loading WAV files.')
 
-if len(x_training_raw) != len(y_training) or len(x_validation_raw) != len(y_validation):
+if len(x_training) != len(y_training) or len(x_validation) != len(y_validation):
 	raise Exception('Dry and wet WAVs must be same length.')
-
-sample_shape = (-1, rnn_timesteps, rnn_features)
-
-x_training = reshape_wav(x_training_raw, sample_shape)
-x_validation = reshape_wav(x_validation_raw, sample_shape)
 	
 model = Sequential()
-model.add(LSTM(rnn_units, dropout=rnn_dropout, return_sequences=True, input_shape=(rnn_timesteps, rnn_features)))
-model.add(Flatten())
+model.add(LSTM(rnn_units, dropout=rnn_dropout, return_sequences=True, input_shape=input_shape))
 model.compile('adam', loss=root_mean_squared_error)
 
 model.fit(x_training, y_training, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_validation, y_validation))
 
-profiler.stop('Done fitting.')
+profiler.stop('Done training.')
