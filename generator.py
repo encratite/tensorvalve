856ff376dcf314f6ad5d8f9beb85550ab5514868
void <git.email.address@googlemail.com>
2019-01-07 01:08:48 +0100

    Switched to data generator.

--- generator.py
+++ generator.py
@@ -1,18 +1,22 @@
+import numpy as np
 from keras.utils import Sequence
 
 class SlidingWindowGenerator(Sequence):
-	def __init__(self, x_wav, y_wav, window_size, output_size):
+	def __init__(self, x_wav, y_wav, input_shape, output_size):
 		self.x_wav = x_wav
 		self.y_wav = y_wav
-		self.window_size = window_size
+		self.input_shape = input_shape
+		self.input_size = input_shape[0] * input_shape[1]
 		self.output_size = output_size
+		assert self.input_size < self.output_size
 
 	def __len__(self):
-		length = len(self.y_wav) - self.window_size - self.output_size
+		length = len(self.x_wav) - self.input_size + 1
 		return length
 
 	def __getitem__(self, index):
-		y_offset = index + self.window_size
-		x = self.x_wav[index : y_offset]
-		y = self.y_wav[y_offset : y_offset + self.output_size]
+		y_offset = index + self.input_size
+		x_linear = self.x_wav[index : y_offset]
+		x = np.reshape(x_linear, self.input_shape)
+		y = self.y_wav[y_offset  - self.output_size : y_offset]
 		return x, y
--- trainer.py
+++ trainer.py
@@ -5,9 +5,9 @@ from keras.layers import LSTM, GRU
 from keras.models import load_model
 
 from loss import root_mean_squared_error
-from wav import reshape_wav
 from callbacks import SaveCallback
 from profiler import Profiler
+from generator import SlidingWindowGenerator
 
 class Trainer:
 	def __init__(self, x_training_wav, y_training_wav, x_validation_wav, y_validation_wav, model_path):
@@ -23,18 +23,14 @@ class Trainer:
 		rnn_dropout = 0.0
 		rnn_units = 32
 		rnn_timesteps = 64
-		rnn_features = rnn_units
+		rnn_features = 32
+
+		output_size = 1
 
 		batch_size = 32
 		epochs = 10000000
 
 		input_shape = (rnn_timesteps, rnn_features)
-	
-		x_training = reshape_wav(self.x_training_wav, input_shape)
-		y_training = reshape_wav(self.y_training_wav, input_shape)
-	
-		x_validation = reshape_wav(self.x_validation_wav, input_shape)
-		y_validation = reshape_wav(self.y_validation_wav, input_shape)
 
 		if os.path.isfile(self.model_path):
 			profiler = Profiler()
@@ -42,8 +38,10 @@ class Trainer:
 			profiler.stop(f'Loaded model from "{self.model_path}".')
 		else:
 			model = Sequential()
-			model.add(rnn_type(rnn_units, dropout=rnn_dropout, return_sequences=True, input_shape=input_shape))
+			model.add(rnn_type(rnn_units, dropout=rnn_dropout, return_sequences=False, input_shape=input_shape))
 			model.compile('adam', loss=root_mean_squared_error)
 
+		training_generator = SlidingWindowGenerator(self.x_training_wav, self.y_training_wav, input_shape, output_size)
+		validation_generator = SlidingWindowGenerator(self.x_validation_wav, self.y_validation_wav, input_shape, output_size)
 		save_callback = SaveCallback(self.model_path)
-		history = model.fit(x_training, y_training, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_validation, y_validation), callbacks=[save_callback])
+		history = model.fit(x=training_generator, y=validation_generator, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_validation, y_validation), callbacks=[save_callback])
